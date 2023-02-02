c775d14b31f381ff1b4bf5eca8a65979dc7e1d95
Peter Ng <peter.ng@itiviti.com>
2023-02-02 15:16:54 +0800

    update indent
    Change-Id: Iab98adab617cd8851d2839a0483fedbca247e31e

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import { useRecoilValue } from "recoil";
 import { userListAtom } from "./GridState";
 
 export default function Footer() {
-	console.log('re-render footer');
-	const users = useRecoilValue(userListAtom);
-	return (<Box>{users.map(u => u.age).reduce((partialSum, a) => partialSum + a, 0)}</Box>);
+    console.log('re-render footer');
+    const users = useRecoilValue(userListAtom);
+    return (<Box>{users.map(u => u.age).reduce((partialSum, a) => partialSum + a, 0)}</Box>);
 }
--- src/Grid.tsx
+++ src/Grid.tsx
@@ -13,26 +13,26 @@ const columns: GridColDef[] = [
 ];
 
 function EditableCell(props: GridRenderCellParams & { fieldName: string }) {
-	const context = useGridApiContext();
-	const {id, value, fieldName} = props;
+    const context = useGridApiContext();
+    const {id, value, fieldName} = props;
 
-	const onChange = React.useCallback((e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-		context.current.updateRows([{ id, [fieldName]: parseInt(e.target.value, 10)}]);
-	}, [context, id, fieldName]);
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        context.current.updateRows([{ id, [fieldName]: parseInt(e.target.value, 10)}]);
+    }, [context, id, fieldName]);
 
-	const updateRow = useRecoilCallback(({snapshot, set}) => async (newRow: any) => {
-		const currentValue: any = await snapshot.getPromise(userSelectorFamily(newRow.id));
-		if (currentValue[fieldName] !== newRow[fieldName]) {
-			set(userSelectorFamily(newRow.id), _ => newRow);
-		}
-	  }, [fieldName]);
+    const updateRow = useRecoilCallback(({snapshot, set}) => async (newRow: any) => {
+        const currentValue: any = await snapshot.getPromise(userSelectorFamily(newRow.id));
+        if (currentValue[fieldName] !== newRow[fieldName]) {
+            set(userSelectorFamily(newRow.id), _ => newRow);
+        }
+      }, [fieldName]);
 
-	const onBlur = React.useCallback((e: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-		updateRow(context.current.getRow(id));
-	}, [context, updateRow, id]);
-	return (
-		<TextField value={value} onChange={onChange} onBlur={onBlur}  />
-	);
+    const onBlur = React.useCallback((e: React.FocusEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        updateRow(context.current.getRow(id));
+    }, [context, updateRow, id]);
+    return (
+        <TextField value={value} onChange={onChange} onBlur={onBlur}  />
+    );
 }
 
 export default function UpdateRowsApiRef() {
@@ -46,11 +46,11 @@ export default function UpdateRowsApiRef() {
     <Box sx={{ width: '100%' }}>
       <Box sx={{ height: 400, mt: 1 }}>
         <DataGridPro
-			experimentalFeatures={{ newEditingApi: true }}
-			rows={users}
-			apiRef={apiRef} 
-			columns={columns}
-		/>
+            experimentalFeatures={{ newEditingApi: true }}
+            rows={users}
+            apiRef={apiRef} 
+            columns={columns}
+        />
       </Box>
     </Box>
   );
